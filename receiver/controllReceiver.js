2985fdcfabfc601c7b3749ad3fce49cb38ad60e1
olivierpoirier <117477661+olivierpoirier@users.noreply.github.com>
2024-05-13 16:18:58 -0400

    comment receiver

--- receiver/controllReceiver.js
+++ receiver/controllReceiver.js
@@ -1,71 +1,49 @@
-import { gameHeight } from "../constants.js";
-import { player, speedOfPlayer } from "../player.js";
+//Chromecast
+//const context = cast.framework.CastReceiverContext.getInstance();
+//const CHANNEL = 'urn:x-cast:gameChannel';
+//let keys = {};
+// let keysDiv;
 
+// context.addCustomMessageListener(CHANNEL, function(customEvent) {
+// //const pos = customEvent.data.msg.split(',');
+// //titleChooseLevel.text = customEvent.data.whereIsGoingPlayerY;
+// //level1text.text = customEvent.data.whereIsGoingPlayerX;
+// if(player.y >= 0){
+//   if(!isTouchedByWallByTop) {
+//     if(keys["38"] || customEvent.data.whereIsGoingPlayerY === "Up"){
+//       player.y -= speedOfPlayer;
+//     }
+//   }
+// }
+// if(player.y < gameHeight - player.height){
+//   if(!isTouchedByWallByBottom) {
+//     if(keys["40"] || customEvent.data.whereIsGoingPlayerY === "Down"){
+//       player.y += speedOfPlayer;
+//     }
+//   }
+// }
+// if (!isTouchedByWallByLeft) {
+//   if(keys["37"] || customEvent.data.whereIsGoingPlayerX === "Left"){
+//     player.x -= speedOfPlayer;
+//   }
+// }
+// if (!isTouchedByWallByRight) {
+//   if(keys["39"] || customEvent.data.whereIsGoingPlayerX === "Right"){
+//     player.x += speedOfPlayer;
+//   }
+// }
+// if(keys['80']) {
+//   console.log('allo')
+//   let aud = new Audio("Audio/menu.mp3");
+//   aud.play();
+// }
 
+// });
 
+// const options = new cast.framework.CastReceiverOptions();
 
-let traductedMessage;
-let whereIsGoingPlayerX;
-let whereIsGoingPlayerY;
+// options.customNamespaces = Object.assign({});
+// options.customNamespaces[CHANNEL] = cast.framework.system.MessageType.JSON;
+// options.disableIdleTimeout = true;
 
-
-
-
-let keys = {};
-let keysDiv;
-
-function keyDown(e) {
-    //console.log(e.keyCode);
-    keys[e.keyCode] = true;
-}
-
-function keyUp(e) {
-    //console.log(e.keyCode);
-    keys[e.keyCode] = false;
-}
-
-function handleMessageFromSender(message) {
-    console.log(message);
-    traductedMessage = JSON.parse(message);
-}
-
-export function castControlManagement(isTouchedByWallByTop, isTouchedByWallByLeft, isTouchedByWallByBottom, isTouchedByWallByRight) {
-    if(traductedMessage != undefined) {
-      whereIsGoingPlayerX = traductedMessage.whereIsGoingPlayerX;
-      whereIsGoingPlayerY = traductedMessage.whereIsGoingPlayerY;
-    }
-    if(player.y >= 0){
-      if(!isTouchedByWallByTop) {
-        if(keys["38"] || whereIsGoingPlayerY === "Up"){
-          player.y -= speedOfPlayer;
-        }
-      }
-    }
-    if(player.y < gameHeight - player.height){
-      if(!isTouchedByWallByBottom || whereIsGoingPlayerY === "Down") {
-        if(keys["40"]){
-          player.y += speedOfPlayer;
-        }
-      }
-    }
-    if (!isTouchedByWallByLeft || whereIsGoingPlayerX === "Left") {
-      if(keys["37"]){
-        player.x -= speedOfPlayer;
-      }
-    }
-    if (!isTouchedByWallByRight || whereIsGoingPlayerX === "Right") {
-      if(keys["39"]){
-        player.x += speedOfPlayer;
-      }
-    }
-    if(keys['80']) {
-      console.log('allo')
-      let aud = new Audio("Audio/menu.mp3");
-      aud.play();
-    }
-  }
-
-
-  window.addEventListener("keydown", keyDown);
-  window.addEventListener("keyup", keyUp);
-  keysDiv = document.querySelector("#keys");
+// context.start(options);
