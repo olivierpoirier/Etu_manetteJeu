9f17ce9565027b56a3c772c7f427ee6ff6ea7ea3
olivierpoirier <117477661+olivierpoirier@users.noreply.github.com>
2024-05-13 17:20:04 -0400

    fix increse difficulty

--- wallsManagement.js
+++ wallsManagement.js
@@ -238,6 +238,7 @@ export function removeWallFromGame(isGameStarted, wall, playerScore, level) {
                 } else {
                     playerScore += 0;
                 }   
+                console.log(comptorHowManyWallsDied)
             }
         }
     } catch(e) {
